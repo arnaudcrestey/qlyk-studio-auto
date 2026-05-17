86ebfe937a8487ca7ac9aa1052b774b905d2ce5b
arnaudcrestey <arnaud.crestey14@gmail.com>
2026-05-17 11:32:30 +0200

    Update core.ts

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -6,14 +6,28 @@ export const ourFileRouter = {
   vehiclePhotos: f({
     image: {
       maxFileSize: '8MB',
-      maxFileCount: 6
-    }
+      maxFileCount: 6,
+    },
   }).onUploadComplete(async ({ file }) => {
     return {
       url: file.url,
-      name: file.name
+      name: file.name,
+      size: file.size,
     };
-  })
+  }),
+
+  volumePhotos: f({
+    image: {
+      maxFileSize: '8MB',
+      maxFileCount: 50,
+    },
+  }).onUploadComplete(async ({ file }) => {
+    return {
+      url: file.url,
+      name: file.name,
+      size: file.size,
+    };
+  }),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
